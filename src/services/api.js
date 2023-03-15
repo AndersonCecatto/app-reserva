e130e2e0a790d9b298141e11151841cdc5066bfd
AndersonCecatto <ander.cecatto@gmail.com>
2023-03-15 13:47:06 -0300

    Alteração Api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
     //baseURL: "http://localhost:14028/api/",
-    baseURL: "http://192.168.3.27/api/",
+    //baseURL: "http://192.168.3.27/api/",
+    baseURL: "http://15.228.235.81/api",
 });
 
 api.defaults.headers.common['Authorization'] = localStorage.getItem('token')
